4f6918f77f7e6287a8698d5561e7ebf21e2fc0b9
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-02-17 22:30:07 +0530

    Update ErrorHanding.js

--- ExpressJS/ErrorHanding.js
+++ ExpressJS/ErrorHanding.js
@@ -4,3 +4,25 @@ const app = express();
 app.get("/home", (req,res) => {
   console.log("Accessed the home page");
 })
+
+// catch 404 error and forward to error handler
+
+app.use((req,res,next) => {
+  const err = new Error('Not Found...');
+  err.satus = 404;
+  
+//   pass error to the next matching route
+  next(err);
+});
+
+// handle another error
+
+app.use((err,req,res,next) => {
+  res.status(err.status || 500);
+  
+  res.render('error', {
+    message : err.message,
+    error : err
+  });
+});
+
