f70909744b1bc1e7be100c8b007ef16c692b0f61
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-04-26 22:02:53 +0530

    Create OOPS.js

---
+++ Javascript( JS )/OOPS.js
@@ -0,0 +1,10 @@
+// class  - template for creating the objects
+class Vehicle{
+    start(){
+        console.log("Starting the vehicles...");
+    }
+}
+
+// creating an object
+let vehicle1  = new Vehicle();
+vehicle1.start();
